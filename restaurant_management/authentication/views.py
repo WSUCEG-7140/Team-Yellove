517e9017c282ab6e389068eba2171f5a792e452c
PravalikaPalakolu <palakolu.2@wright.edu>
2023-07-11 09:59:11 +0530

    Refactored code to incorporate login feature and implemented JWT token-based authorization for all APIs.

--- restaurant_management/authentication/views.py
+++ restaurant_management/authentication/views.py
@@ -3,14 +3,45 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import CustomUser
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+
+# @api_view(['POST'])
+# def create_user(request):
+#     serializer = UserSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
+@permission_classes([])
 def create_user(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        user = serializer.save()
+
+        # Generate JWT tokens for the user
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        response_data = {
+            'access_token': access_token,
+            'user': serializer.data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
 
 @api_view(['GET'])
 def get_user_list(request):
